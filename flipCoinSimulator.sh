b9ee78076ea518c866361c310ab8514c94bcfb5f
saranamsairakesh <saranamsairakesh@gmail.com>
2021-03-29 15:12:52 +0530

    Refactor: Flip Coin Multiple Times with Tie Condition

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,27 +1,52 @@
 headsCount=0
 tailsCount=0
 
-while [ 5 -eq 5 ]
+function flipCoin(){
+	flip=$1
+   if [ $flip -eq 1 ]
+   then
+      headsCount=$(($headsCount+1))
+   else
+      tailsCount=$(($tailsCount+1))
+   fi
+	echo $headsCount","$tailsCount
+}
+
+while [ $headsCount -ne 21 ] && [ $tailsCount -ne 21 ]
 do
-	flip=$(($RANDOM%2))
-	if [ $flip -eq 1 ]
-	then
-		headsCount=$(($headsCount+1))
-	else
-		tailsCount=$(($tailsCount+1))
-	fi
-	if [ $headsCount -eq 21 ] || [ $tailsCount -eq 21 ]
-	then
-		break
-	fi
+	flipResult="$( flipCoin $(($RANDOM%2)) )"
+	headsCount=$( echo $flipResult | awk -F , '{print $1}')
+	tailsCount=$( echo $flipResult | awk -F , '{print $2}')
 done
 
-if [ $headsCount -eq $tailsCount ]
+diff=$(($headsCount-$tailsCount))
+echo $headsCount","$tailsCount
+
+function winner(){
+	diff=$1
+	if [ $diff -gt 0 ]
+   then
+      echo "Heads win on Tails by "$diff
+   else
+      echo "Tails win on Heads by "$((-1*$diff))
+   fi
+}
+
+if [ $diff -eq 0 ]
 then
 	echo "Heads equals to Tails. So it's a TIE"
-elif [ $headsCount -gt $tailsCount ]
-then
-	echo "Heads win on Tails by "$(($headsCount-$tailsCount))
+	while [ $diff -ne 2 ] && [ $diff -ne -2 ]
+	do
+		flip=$(($RANDOM%2))
+   	if [ $flip -eq 1 ]
+   	then
+      	headsCount=$(($headsCount+1))
+   	else
+   	   tailsCount=$(($tailsCount+1))
+	   fi
+		diff=$(($headsCount-$tailsCount))
+	done
+	echo "$( winner $(($diff)) )"
 else
-	echo "Tails win on Heads by "$(($tailsCount-$headsCount))
+	echo "$( winner $(($diff)) )"
 fi
