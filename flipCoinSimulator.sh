7953e4d606800b166f7c8eba5a4e4552ac399e66
saranamsairakesh <saranamsairakesh@gmail.com>
2021-03-29 14:14:14 +0530

    Added: Flip Coin Multiple Times

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,7 +1,15 @@
-flip=$(($RANDOM%2))
-if [ $flip -eq 1 ]
-then
-	echo "HEADS"
-else
-	echo "TAILS"
-fi
+headsCount=0
+tailsCount=0
+
+for((i=1;i<=10;i++))
+do
+	flip=$(($RANDOM%2))
+	if [ $flip -eq 1 ]
+	then
+		headsCount=$(($headsCount+1))
+	else
+		tailsCount=$(($tailsCount+1))
+	fi
+done
+
+echo "In 10 flips Heads Count= "$headsCount", Tails Count= "$tailsCount
